a9779376a7afa315516b945a05b9ea0081633292
con7ext <57825968+con7ext@users.noreply.github.com>
2020-04-18 12:33:28 +0700

    Update Example.php

--- Example.php
+++ Example.php
@@ -7,6 +7,7 @@ $curl::MakeRequests();
 // $curl::SetHeaders(array("User-Agent: Mozilla")); // if u want set a headers
 // $curl::Follow(); // If U want to follow location
 // $curl::Cookies(); // if u want to save your session
+// $curl::SetTimeout(5); // if u want set request timeout
 $curl::GET(); // $curl::POST('user=user&pass=pass');
 echo $curl::Response()->body; // Print body response
 echo $curl::Response()->status_code; // Print Status code
