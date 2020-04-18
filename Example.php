8b20e862e64b3d2c9727c57ca90523161447100d
con7ext <57825968+con7ext@users.noreply.github.com>
2020-04-18 12:31:55 +0700

    Create Example.php

---
+++ Example.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'WibuCurl.php';
+
+$curl = new Curl();
+$curl::$URL = 'https://rintod.dev';
+$curl::MakeRequests();
+// $curl::SetHeaders(array("User-Agent: Mozilla")); // if u want set a headers
+// $curl::Follow(); // If U want to follow location
+// $curl::Cookies(); // if u want to save your session
+$curl::GET(); // $curl::POST('user=user&pass=pass');
+echo $curl::Response()->body; // Print body response
+echo $curl::Response()->status_code; // Print Status code
+print_r($curl::Response()->headers); /// Print all headers (array)
+echo $curl::Response()->headers->location; // Print location headers
